84caf348ee129ca1881f2eae34a67186add937ef
KingRainbow44 <27646710+KingRainbow44@users.noreply.github.com>
2020-06-04 15:39:52 -0700

    Rewrote VanishTask.php

--- src/ErkamKahriman/Vanish/VanishTask.php
+++ src/ErkamKahriman/Vanish/VanishTask.php
@@ -7,30 +7,26 @@ use pocketmine\Server;
 use ErkamKahriman\Vanish\Vanish;
 
 class VanishTask extends Task {
+
+    /** @var Vanish */
     private $plugin;
+
     public function __construct(Vanish $plugin) {
         $this->plugin = $plugin;
-        parent::__construct($plugin);
     }
+
     public function onRun(int $currentTick) : void {
-        foreach (Server::getInstance()->getOnlinePlayers() as $player){
-            if($player->spawned()){
-            if(Vanish::getInstance()->vanish[$player->getName()] == true){
-                foreach (Server::getInstance()->getOnlinePlayers() as $players){
-                    $players->hidePlayer($player);
-                    }
-                } else {
-         foreach (Server::getInstance()->getOnlinePlayers() as $player){
-             if($player->hasPermission("supervanish.see")){
-                    if(Vanish::getInstance()->vanish[$player->getName()] == false){
-                       foreach (Server::getInstance()->getOnlinePlayers() as $players){
-                           $players->showPlayer($player);
-}
-}
-}
-}
-}
-}
-}
-}
+
+        $vanished = Vanish::$vanished;
+
+        foreach(Server::getInstance()->getOnlinePlayers() as $player) {
+            
+            if($player->hasPermission("supervanish.see")) return;
+            
+            foreach($vanished as $vanishedPlayer) {
+                $player->hidePlayer($vanishedPlayer);
+            }
+        }
+
+    }
 }
