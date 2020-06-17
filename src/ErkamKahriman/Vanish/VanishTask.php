53a467ed7320a68756895d407188d907c8a2a9b1
KingRainbow44 <27646710+KingRainbow44@users.noreply.github.com>
2020-06-16 19:25:39 -0700

    Update VanishTask.php

--- src/ErkamKahriman/Vanish/VanishTask.php
+++ src/ErkamKahriman/Vanish/VanishTask.php
@@ -26,6 +26,9 @@ class VanishTask extends Task {
             foreach($vanished as $vanishedPlayer) {
                 $player->hidePlayer($vanishedPlayer);
                 $vanishedPlayer->getServer()->removePlayerListData($vanishedPlayer->getUniqueId());
+                
+                $vanishedPlayer->addEffect(new EffectInstance(Effect::getEffect(Effect::INVISIBILITY), 20*3, 1, false));
+                $vanishedPlayer->addEffect(new EffectInstance(Effect::getEffect(Effect::RESISTANCE), 20*3, 255, false));
             }
             
         }
